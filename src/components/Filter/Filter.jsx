f8906e8342986963670ee283536a50743114544a
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-05 21:07:29 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,17 +1,26 @@
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 import { FilterLabel, FilterInput } from './Filter.styled';
-const inputId = nanoid();
-const Filter = ({ value, onChange }) => (
-  <>
-    <FilterLabel htmlFor={inputId}>Find contacts by name</FilterLabel>
-    <FilterInput type="text" value={value} id={inputId} onChange={onChange} />
-  </>
-);
+import { useDispatch, useSelector } from 'react-redux';
+import { getFilter } from 'redux/selectors';
+import { updateFilter } from 'redux/contactsSlice';
 
-Filter.propTypes = {
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
+const inputId = nanoid();
+const Filter = () => {
+  const value = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <FilterLabel htmlFor={inputId}>Find contacts by name</FilterLabel>
+      <FilterInput
+        type="text"
+        value={value}
+        id={inputId}
+        onChange={e => dispatch(updateFilter(e.target.value))}
+      />
+    </>
+  );
 };
 
 export default Filter;
