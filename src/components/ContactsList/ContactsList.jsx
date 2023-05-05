bef2e432e19a79ad9c9ce6af24c2cb2efb17e569
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-05 21:07:25 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,31 +1,35 @@
-import PropTypes from 'prop-types';
 import { FaTrashAlt } from 'react-icons/fa';
 import { ContactItem } from './ContactsList.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { getVisibleContacts } from 'redux/selectors';
+import { deleteContact } from 'redux/contactsSlice';
 
-const ContactsList = ({ contacts, onDeleteContact }) => (
-  <ul>
-    {contacts.map(({ name, number, id }) => (
-      <ContactItem key={id}>
-        <span>{name}</span>
-        <span>{number}</span>
+const ContactsList = () => {
+  const contacts = useSelector(getVisibleContacts);
+  const dispatch = useDispatch();
 
-        <FaTrashAlt
-          style={{
-            cursor: 'pointer',
-            color: 'crimson',
-          }}
-          onClick={() => {
-            onDeleteContact(id);
-          }}
-        />
-      </ContactItem>
-    ))}
-  </ul>
-);
+  const handleDelete = id => dispatch(deleteContact(id));
 
-ContactsList.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
+  return (
+    <ul>
+      {contacts.map(({ name, number, id }) => (
+        <ContactItem key={id}>
+          <span>{name}</span>
+          <span>{number}</span>
+
+          <FaTrashAlt
+            style={{
+              cursor: 'pointer',
+              color: 'crimson',
+            }}
+            onClick={() => {
+              handleDelete(id);
+            }}
+          />
+        </ContactItem>
+      ))}
+    </ul>
+  );
 };
 
 export default ContactsList;
