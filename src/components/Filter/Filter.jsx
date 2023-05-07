a7d3db4d20b141d6ced83039f731fff0717c875a
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-07 12:52:14 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 import { FilterLabel, FilterInput } from './Filter.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { getFilter } from 'redux/selectors';
-import { updateFilter } from 'redux/contactsSlice';
+import { updateFilter } from 'redux/filterSlice';
 
 const inputId = nanoid();
 const Filter = () => {
