b8d2722f9c07230948e1b408ce0dd175012ec978
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-05-07 12:52:05 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Container from './Container';
-import ContactForm from './ContactForm';
+import ContactForm from './ContactForm/ContactForm';
 import ContactsList from './ContactsList';
 import Filter from './Filter';
 import { MainTitle, SecondTitle } from './Container/Container.styled';
